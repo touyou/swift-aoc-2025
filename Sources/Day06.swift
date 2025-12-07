c5ba447e8bb24714d18257250d42a755126b35fb
touyou <465697+touyou@users.noreply.github.com>
2025-12-07 13:39:54 +0900

    feat: complete day06

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -13,15 +13,15 @@ struct Day06: AdventDay {
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Int {
-    guard let entity = entities.first else { return 0 }
-    let operations = entity.popLast()?.split(separator: " ")?.filter { $0.isNotEmpty }.compactMap { String($0) } ?? []
-    let numbers = entity.map { $0.split(separator: " ").filter { $0.isNotEmpty }.compactMap { Int($0) } }
+    guard var entity = entities.first else { return 0 }
+    let operations = entity.popLast()?.split(separator: " ").compactMap { String($0) } ?? []
+    let numbers = entity.map { $0.split(separator: " ").compactMap { Int($0) } }
     var answer = 0
     for i in 0..<operations.count {
       if operations[i] == "*" {
-        answer += numbers.reduce(1) { result, array in result *= array[i] }
+        answer += numbers.reduce(1) { result, array in result * array[i] }
       } else {
-        answer += numbers.reduce(0) { result, array in result += array[i] }
+        answer += numbers.reduce(0) { result, array in result + array[i] }
       }
     }
 
@@ -29,8 +29,43 @@ struct Day06: AdventDay {
   }
 
   // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    return 0
+  func part2() -> Int {
+    guard var entity = entities.first else { return 0 }
+    let operations = entity.popLast() ?? ""
+    let numbers = entity
+    var answer = 0
+    var tempAnswer = 0
+    var lastOp = ""
+    for i in 0..<operations.count {
+      var numStr = ""
+      let op = String(operations.dropFirst(i).prefix(1))
+      for j in 0..<numbers.count {
+        let char = String(numbers[j].dropFirst(i).prefix(1))
+        if !char.isEmpty {
+          numStr += char
+        }
+      }
+      let num = Int(numStr.trimmingCharacters(in: .whitespacesAndNewlines))
+      if num == nil {
+        answer += tempAnswer
+        tempAnswer = 0
+        continue
+      }
+      if op == "*" {
+        tempAnswer = num ?? 1
+        lastOp = "*"
+      } else if op == "+" {
+        tempAnswer = num ?? 0
+        lastOp = "+"
+      } else if lastOp == "*" {
+        tempAnswer *= num ?? 1
+      } else if lastOp == "+" {
+        tempAnswer += num ?? 0
+      }
+    }
+    if tempAnswer != 0 {
+      answer += tempAnswer
+    }
+    return answer
   }
 }
--- Tests/Day06.swift
+++ Tests/Day06.swift
@@ -8,9 +8,9 @@ struct Day06Tests {
   // Smoke test data provided in the challenge question
   let testData = """
     123 328  51 64 
-    45 64  387 23 
-    6 98  215 314
-    *   +   *   +  
+     45 64  387 23 
+      6 98  215 314
+    *   +   *   +   
     """
 
   @Test func testPart1() async throws {
@@ -20,6 +20,6 @@ struct Day06Tests {
 
   @Test func testPart2() async throws {
     let challenge = Day06(data: testData)
-    #expect(String(describing: challenge.part2()) == "32000")
+    #expect(String(describing: challenge.part2()) == "3263827")
   }
 }
